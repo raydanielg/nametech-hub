554e305e13d869f60e12c5a033c3d0d1c759948b
raydanielg <airezra2+1@gmail.com>
2026-04-28 01:26:42 +0300

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProgramSeeder::class,
+            BlogSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
     }
