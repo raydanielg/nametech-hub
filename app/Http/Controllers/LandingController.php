e9c287d73306ab0e181c2db73ae53c035f796a38
raydanielg <airezra2+1@gmail.com>
2026-04-28 01:11:26 +0300

    commit

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Program;
 use App\Models\Partner;
 use App\Models\Announcement;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class LandingController extends Controller
@@ -14,7 +15,8 @@ class LandingController extends Controller
         $programs = Program::where('is_active', true)->get();
         $partners = Partner::where('is_active', true)->orderBy('order')->get();
         $announcement = Announcement::where('is_active', true)->latest()->first();
+        $testimonials = Testimonial::where('is_active', true)->get();
         
-        return view('landing.index', compact('programs', 'partners', 'announcement'));
+        return view('landing.index', compact('programs', 'partners', 'announcement', 'testimonials'));
     }
 }
