3365442d1d843a65238390ec23ba243fc0896aa8
raydanielg <airezra2+1@gmail.com>
2026-04-28 06:30:11 +0300

    commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'has.role' => \App\Http\Middleware\EnsureHasRole::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'startup' => \App\Http\Middleware\StartupMiddleware::class,
