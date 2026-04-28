23f75226667b82fa72a1fce4fe1f2c5766a1db8f
raydanielg <airezra2+1@gmail.com>
2026-04-28 07:47:02 +0300

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,20 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
