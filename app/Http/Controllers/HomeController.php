0a9a7e8e1515b5ff90325dd911a7239cbf5583a2
raydanielg <airezra2+1@gmail.com>
2026-04-28 08:30:05 +0300

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,10 +19,40 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function index()
     {
-        return view('home');
+        $user = auth()->user();
+
+        if ($user->hasRole('super_admin')) {
+            return redirect()->route('admin.dashboard.main');
+        }
+
+        if ($user->hasRole('hub_director')) {
+            return redirect()->route('hub.dashboard.main');
+        }
+
+        if ($user->hasRole('studio_director')) {
+            return redirect()->route('studio.dashboard.main');
+        }
+
+        if ($user->hasRole('startup_founder')) {
+            return redirect()->route('founder.dashboard.main');
+        }
+
+        if ($user->hasRole('mentor')) {
+            return redirect()->route('mentor.dashboard.main');
+        }
+
+        if ($user->hasRole('investor')) {
+            return redirect()->route('investor.dashboard.main');
+        }
+
+        if ($user->hasRole('student')) {
+            return redirect()->route('student.dashboard.main');
+        }
+
+        return redirect()->route('dashboard');
     }
 }
