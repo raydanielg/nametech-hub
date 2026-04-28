4120df9b86067c2d08d1a867ce8adaf8c0b5133e
raydanielg <airezra2+1@gmail.com>
2026-04-28 06:42:22 +0300

    commit

---
+++ app/Http/Controllers/StartupFounder/FounderDashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\StartupFounder;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FounderDashboardController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/StudioDirector/StudioDashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\StudioDirector;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StudioDashboardController extends Controller
+{
+    //
+}
