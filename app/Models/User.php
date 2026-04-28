57e3d5d97f6e8ef9a4a8aac2acf47333ce09592c
raydanielg <airezra2+1@gmail.com>
2026-04-28 06:51:39 +0300

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Spatie\Permission\Traits\HasRoles;
+
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +20,16 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'id',
         'email',
         'password',
+        'first_name',
+        'last_name',
+        'phone',
+        'profile_picture_url',
+        'role',
+        'status',
+        'email_verified',
     ];
 
     /**
@@ -39,6 +48,16 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'email_verified' => 'boolean',
+        'phone_verified' => 'boolean',
+        'last_login_at' => 'datetime',
     ];
+
+    /**
+     * Get the user's full name.
+     */
+    public function getNameAttribute()
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
