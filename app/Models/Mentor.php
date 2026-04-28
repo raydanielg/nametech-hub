be878beca6706ab3adaf66798e1f98445e7d605d
raydanielg <airezra2+1@gmail.com>
2026-04-28 07:54:46 +0300

    commit

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Mentor extends Model
 {
     use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'expertise_areas',
+        'years_of_experience',
+        'current_role',
+        'company',
+        'max_startups',
+        'is_active',
+        'bio',
+        'linkedin_url',
+    ];
+
+    protected $casts = [
+        'expertise_areas' => 'array',
+        'availability' => 'array',
+        'is_active' => 'boolean',
+    ];
 }
