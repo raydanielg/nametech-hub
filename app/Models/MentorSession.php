9d8caa34547be45e5e9a7e337b40cb4120d910fa
raydanielg <airezra2+1@gmail.com>
2026-04-28 07:53:44 +0300

    commit

--- app/Models/MentorSession.php
+++ app/Models/MentorSession.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class MentorSession extends Model
 {
     use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'id',
+        'mentor_id',
+        'startup_id',
+        'mentee_id',
+        'topic',
+        'description',
+        'session_date',
+        'start_time',
+        'end_time',
+        'status',
+        'meeting_link',
+        'mentor_notes',
+        'mentee_feedback',
+    ];
+
+    protected $casts = [
+        'session_date' => 'date',
+    ];
 }
