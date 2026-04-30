27f64b0fc9c03164768e916ead2c3a3670f68a0d
raydanielg <airezra2+1@gmail.com>
2026-04-30 19:14:47 +0300

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!app()->runningInConsole()) {
-            config(['app.asset_url' => '/public']);
-        }
+        //
     }
 }
